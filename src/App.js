e8ce320c6714d4f36f9e7351ab5041792bd3c4ef
balausa <52880784+balausa@users.noreply.github.com>
2022-12-14 22:45:57 +0600

    added rtk_query_mutations(addProduct, deleteProduct) and autorefresh(invalidatesTags)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import {useState} from 'react';
-import { useGetGoodsQuery } from './redux';
+import { useState } from 'react';
+import { useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation } from './redux';
 
 function App() {
-  const [count, setCount]=useState('');
-  const { data = [], error, isLoading } = useGetGoodsQuery();
+  const [count, setCount] = useState('');
+  const [newProduct, setNewProduct] = useState('');
+  const { data = [], error, isLoading } = useGetGoodsQuery(count);
+  const [addProduct, { isError }] = useAddProductMutation();
+  const [deleteProduct] = useDeleteProductMutation();
 
+  const handleAddProduct = async () => {
+    if (newProduct) {
+      await addProduct({ name: newProduct }).unwrap();
+      setNewProduct('');
+    }
+  }
+
+  const handleDeleteProduct = async (id) => {
+    await deleteProduct(id).unwrap();
+  }
   if (isLoading) return <h1>Loading...</h1>
 
   return (
     <div>
+      <div>
+        <input
+          type="text"
+          value={newProduct}
+          onChange={(e) => setNewProduct(e.target.value)} />
+        <button onClick={handleAddProduct}>Add product</button>
+      </div>
+      <div>
+        <select value={count} onChange={(e) => setCount(e.target.value)}>
+          <option value="">All</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
       <ul>
         {data.map(item => (
-          <li key={item.id}>
+          <li key={item.id} onClick={() => handleDeleteProduct(item.id)}>
             {item.name}
           </li>
         ))}
