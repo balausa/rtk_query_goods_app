fd4f3d80f303203de5faed4eeaadfe247f449daf
balausa <52880784+balausa@users.noreply.github.com>
2022-12-14 22:09:34 +0600

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import { useGetGoodsQuery } from './redux';
 
 function App() {
+  const [count, setCount]=useState('');
+  const { data = [], error, isLoading } = useGetGoodsQuery();
+
+  if (isLoading) return <h1>Loading...</h1>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            {item.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
